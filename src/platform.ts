3a543b5ab31e94fdde5798c6c3686a409cc38366
Michael Hawkshaw <228663+mhawkshaw@users.noreply.github.com>
2021-05-12 22:03:32 +0200

    Initial version

--- src/platform.ts
+++ src/platform.ts
@@ -1,25 +1,36 @@
 import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
 
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
-import { ExamplePlatformAccessory } from './platformAccessory';
+import { EnviroplusSensor } from './platformAccessory';
 
 /**
- * HomebridgePlatform
- * This class is the main constructor for your plugin, this is where you should
- * parse the user config and discover/register accessories with Homebridge.
+ * EnviroplusPlatform
+ * Here the user config is loaded and the Enviroplus accessories are created
  */
-export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
+export class EnviroplusPlatform implements DynamicPlatformPlugin {
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
 
   // this is used to track restored cached accessories
   public readonly accessories: PlatformAccessory[] = [];
 
+  private configProvided() {
+    return this.config.server != null && this.config.serial != null && this.config.excellent != null && this.config.good != null && this.config.fair != null && this.config.inferior != null && this.config.poor != null;
+  }
+
   constructor(
     public readonly log: Logger,
     public readonly config: PlatformConfig,
     public readonly api: API,
   ) {
+
+    // Checks whether a configuration is provided, otherwise the plugin should not be initialized
+    if (!this.configProvided()) {
+      log.error("Not all configuration provided!");
+      log.info("Server for enviroment data is required along with the upper limits for the air quality and the serial number of the enviroplus device");
+      return;
+    }
+
     this.log.debug('Finished initializing platform:', this.config.name);
 
     // When this event is fired it means Homebridge has restored all cached accessories from disk.
@@ -37,7 +48,7 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
    * This function is invoked when homebridge restores cached accessories from disk at startup.
    * It should be used to setup event handlers for characteristics and update respective values.
    */
-  configureAccessory(accessory: PlatformAccessory) {
+  configureAccessory(accessory: PlatformAccessory): void {
     this.log.info('Loading accessory from cache:', accessory.displayName);
 
     // add the restored accessory to the accessories cache so we can track if it has already been registered
@@ -45,33 +56,24 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
   }
 
   /**
-   * This is an example method showing how to register discovered accessories.
-   * Accessories must only be registered once, previously created accessories
+   * Register discovered accessories.
+   * Accessories are only registered once, previously created accessories
    * must not be registered again to prevent "duplicate UUID" errors.
    */
-  discoverDevices() {
+  discoverDevices(): void {
 
-    // EXAMPLE ONLY
-    // A real plugin you would discover accessories from the local network, cloud services
-    // or a user-defined array in the platform config.
-    const exampleDevices = [
+    const devices = [
       {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
+        uniqueId: this.config.serial,
+        displayName: 'Enviroplus',
       },
     ];
 
     // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
+    for (const device of devices) {
 
-      // generate a unique id for the accessory this should be generated from
-      // something globally unique, but constant, for example, the device serial
-      // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
+      // generate a unique id for the accessory based on the provided serial number
+      const uuid = this.api.hap.uuid.generate(device.uniqueId);
 
       // see if an accessory with the same uuid has already been registered and restored from
       // the cached devices we stored in the `configureAccessory` method above
@@ -81,24 +83,15 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
         // the accessory already exists
         this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
 
-        // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-        // existingAccessory.context.device = device;
-        // this.api.updatePlatformAccessories([existingAccessory]);
-
         // create the accessory handler for the restored accessory
         // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, existingAccessory);
-
-        // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
-        // remove platform accessories when no longer present
-        // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
-        // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
+        new EnviroplusSensor(this, existingAccessory);
       } else {
         // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
+        this.log.info('Adding new accessory:', device.displayName);
 
         // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
+        const accessory = new this.api.platformAccessory(device.displayName, uuid);
 
         // store a copy of the device object in the `accessory.context`
         // the `context` property can be used to store any data about the accessory you may need
@@ -106,7 +99,7 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
 
         // create the accessory handler for the newly create accessory
         // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, accessory);
+        new EnviroplusSensor(this, accessory);
 
         // link the accessory to your platform
         this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,141 +1,174 @@
-import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
+import { Service, PlatformAccessory } from 'homebridge';
 
-import { ExampleHomebridgePlatform } from './platform';
+import { EnviroplusPlatform } from './platform';
+
+import { XMLHttpRequest } from 'xmlhttprequest-ts';
 
 /**
- * Platform Accessory
- * An instance of this class is created for each accessory your platform registers
- * Each accessory may expose multiple services of different service types.
+ * Enviroplus Accessory
+ * An instance of this class is created for each accessory registered (in this case only one)
+ * The Enviroplus accessory exposes the services of temperature, air quality and humidity
  */
-export class ExamplePlatformAccessory {
-  private service: Service;
+export class EnviroplusSensor {
+  private airQualityService: Service;
+  private humidityService: Service;
+  private temperatureService: Service;
 
-  /**
-   * These are just used to create a working example
-   * You should implement your own code to track the state of your accessory
-   */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
+  // User to store the sensor data for quick retrieval
+  private sensorData = {
+    airQuality: this.platform.Characteristic.AirQuality.UNKNOWN,
+    temperature: -270,
+    humidity: 0,
+    P2: 0,
+    P1: 0,
   };
 
+  /**
+   * Gets the Enviroplus JSON data from the specified URI
+   */
+   getEnviroValues(url: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const req = new XMLHttpRequest();
+      req.open('GET', url);
+
+      req.onload = () => {
+        if (req.status === 200) {
+          resolve(req.responseText);
+        } else {
+          reject(Error(req.statusText));
+        }
+      };
+
+      req.onerror = () => {
+        reject(Error('Network Error'));
+      };
+
+      req.send();
+    });
+  }
+
+  updateAirQuality(): void {
+    this.getEnviroValues(this.platform.config.server).then(response => {
+      const jsonData = JSON.parse(response);
+
+      this.sensorData.temperature = jsonData.temperature;
+      this.sensorData.humidity = jsonData.humidity;
+      this.sensorData.P2 = jsonData.P2;
+      this.sensorData.P1 = jsonData.P1;
+
+      this.platform.log.debug('Temperature:', this.sensorData.temperature);
+      this.platform.log.debug('Humidity:', this.sensorData.humidity);
+      this.platform.log.debug('P2:', this.sensorData.P2);
+      this.platform.log.debug('P1:', this.sensorData.P1);
+
+      if (this.sensorData.P2 <= this.platform.config.excellent) {
+        this.sensorData.airQuality = this.platform.Characteristic.AirQuality.EXCELLENT;
+      }
+      else if (this.sensorData.P2 > this.platform.config.excellent && this.sensorData.P2 <= this.platform.config.good) {
+        this.sensorData.airQuality = this.platform.Characteristic.AirQuality.GOOD;
+      }
+      else if (this.sensorData.P2 > this.platform.config.good && this.sensorData.P2 <= this.platform.config.fair) {
+        this.sensorData.airQuality = this.platform.Characteristic.AirQuality.FAIR;
+      }
+      else if (this.sensorData.P2 > this.platform.config.fair && this.sensorData.P2 <= this.platform.config.inferior) {
+        this.sensorData.airQuality = this.platform.Characteristic.AirQuality.INFERIOR;
+      }
+      else if (this.sensorData.P2 > this.platform.config.poor) {
+        this.sensorData.airQuality = this.platform.Characteristic.AirQuality.POOR;
+      }
+    }).catch(error => {
+      this.platform.log.error("Unable to obtain data from Enviro+ Server", this.platform.config.server, error);
+    });
+  }
+
   constructor(
-    private readonly platform: ExampleHomebridgePlatform,
+    private readonly platform: EnviroplusPlatform,
     private readonly accessory: PlatformAccessory,
   ) {
 
     // set accessory information
-    this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
-      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+    const accessoryInfo: Service | undefined = this.accessory.getService(this.platform.Service.AccessoryInformation);
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
-    // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
+    if (accessoryInfo != null) {
+      accessoryInfo.setCharacteristic(this.platform.Characteristic.Manufacturer, 'Pimoroni')
+        .setCharacteristic(this.platform.Characteristic.Model, 'EnviroPlus')
+        .setCharacteristic(this.platform.Characteristic.SerialNumber, this.platform.config.serial);
+    }
+
+    this.airQualityService = this.accessory.getService(this.platform.Service.AirQualitySensor) || this.accessory.addService(this.platform.Service.AirQualitySensor);
+    this.temperatureService = this.accessory.getService(this.platform.Service.TemperatureSensor) || this.accessory.addService(this.platform.Service.TemperatureSensor);
+    this.humidityService = this.accessory.getService(this.platform.Service.HumiditySensor) || this.accessory.addService(this.platform.Service.HumiditySensor);
 
     // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
+    this.airQualityService.setCharacteristic(this.platform.Characteristic.Name, "Air Quality");
+    this.temperatureService.setCharacteristic(this.platform.Characteristic.Name, "Temperature");
+    this.humidityService.setCharacteristic(this.platform.Characteristic.Name, "Humidity");
 
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
+    // register handlers
+    this.airQualityService.getCharacteristic(this.platform.Characteristic.AirQuality)
+      .onGet(this.handleAirQualityGet.bind(this));
+    this.airQualityService.getCharacteristic(this.platform.Characteristic.PM10Density)
+      .onGet(this.handlePM10DensityGet.bind(this));
+    this.airQualityService.getCharacteristic(this.platform.Characteristic.PM2_5Density)
+      .onGet(this.handlePM2_5DensityGet.bind(this));
+    this.temperatureService.getCharacteristic(this.platform.Characteristic.CurrentTemperature)
+      .onGet(this.handleTemperatureGet.bind(this));
+    this.humidityService.getCharacteristic(this.platform.Characteristic.CurrentRelativeHumidity)
+      .onGet(this.handleHumidityGet.bind(this));
 
-    // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
-
-    // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .onSet(this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
-
-    /**
-     * Creating multiple services of the same type.
-     *
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     *
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
-     */
-
-    // Example: add two "motion sensor" services to the accessory
-    const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
-
-    const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
+    // Do an initial get of the air quality data
+    this.updateAirQuality();
 
     /**
      * Updating characteristics values asynchronously.
      *
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     *
+     * For performance reasons, we get the data from the Enviro+ server periodically and store it locally in our variables
+     * then push the updates to HomeKit. Then when HomeKit requests the data, we used the locally stored data instead.
      */
-    let motionDetected = false;
+    const ms_to_minutes = 60000;
+
     setInterval(() => {
-      // EXAMPLE - inverse the trigger
-      motionDetected = !motionDetected;
-
+      this.updateAirQuality();
       // push the new value to HomeKit
-      motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-      motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
-
-      this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-      this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    }, 10000);
-  }
-
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
-  async setOn(value: CharacteristicValue) {
-    // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
-
-    this.platform.log.debug('Set Characteristic On ->', value);
+      this.airQualityService.updateCharacteristic(this.platform.Characteristic.AirQuality, this.sensorData.airQuality);
+      this.airQualityService.updateCharacteristic(this.platform.Characteristic.PM10Density, this.sensorData.P1);
+      this.airQualityService.updateCharacteristic(this.platform.Characteristic.PM2_5Density, this.sensorData.P2);
+      this.temperatureService.updateCharacteristic(this.platform.Characteristic.CurrentTemperature, this.sensorData.temperature);
+      this.humidityService.updateCharacteristic(this.platform.Characteristic.CurrentRelativeHumidity, this.sensorData.humidity);
+    }, this.platform.config.interval * ms_to_minutes);
   }
 
   /**
    * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   *
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   *
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
-
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
+   * Here we use the locally stored data for performance reasons and also to avoid sending too many requests to the Enviro+ server
    */
-  async getOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On;
+  async handleAirQualityGet(): Promise<number> {
+    this.platform.log.debug('Air Quality ->', this.sensorData.airQuality);
 
-    this.platform.log.debug('Get Characteristic On ->', isOn);
-
-    // if you need to return an error to show the device as "Not Responding" in the Home app:
-    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
-
-    return isOn;
+    return this.sensorData.airQuality;
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
-   */
-  async setBrightness(value: CharacteristicValue) {
-    // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
+  async handlePM10DensityGet(): Promise<number> {
+    this.platform.log.debug('PM10 Density ->', this.sensorData.P1);
 
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
+    return this.sensorData.P1;
   }
 
+  async handlePM2_5DensityGet(): Promise<number> {
+    this.platform.log.debug('PM2.5 Density ->', this.sensorData.P2);
+
+    return this.sensorData.P2;
+  }
+
+  async handleTemperatureGet(): Promise<number> {
+    this.platform.log.debug('Temperature ->', this.sensorData.temperature);
+
+    return this.sensorData.temperature;
+  }
+
+  async handleHumidityGet(): Promise<number> {
+    this.platform.log.debug('Humidity ->', this.sensorData.humidity);
+
+    return this.sensorData.humidity;
+  }
 }
